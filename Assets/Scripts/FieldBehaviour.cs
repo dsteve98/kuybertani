3eb5f9f06aebe1408b3adbe027bcefadac7fd815
Stv <dan.steve2910@gmail.com>
2019-05-19 00:05:21 +0700

    change growth state after 2x watering

--- Assets/Scripts/FieldBehaviour.cs
+++ Assets/Scripts/FieldBehaviour.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class FieldBehaviour : MonoBehaviour
 {
-    public Sprite[] plantSprites;
-    public int state = 0;
 
-    void Awake()
-    {
-        
-    }
+    [SerializeField]
+    private Sprite[] plantSprites;
+    private int state = 0;
+    private int waterlevel = 4;
+    private int planttype;
+    private int progresslimit = 2;
+    private int progress = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -28,12 +30,70 @@ public class FieldBehaviour : MonoBehaviour
     {
         if (other.CompareTag("hitbox"))
         {
-            if(PlayerMovement.currentEquip == "seed" && state == 0)
+            if(state == 0)
             {
-                GetComponent<SpriteRenderer>().sprite = plantSprites[0];
-                //Debug.Log(plantSprites.Length);
-                //Debug.Log(state);
+                Debug.Log(PlayerMovement.main.seed[2]);
+                if (PlayerMovement.main.currentEquip == "tomatoseed" && PlayerMovement.main.seed[0] != 0)
+                {
+                    GetComponent<SpriteRenderer>().sprite = plantSprites[0];
+                    planttype = 0;
+                    state = 1;
+                    Debug.Log("plant tomato");
+                }
+                else if (PlayerMovement.main.currentEquip == "potatoseed" && PlayerMovement.main.seed[1] != 0)
+                {
+                    GetComponent<SpriteRenderer>().sprite = plantSprites[1];
+                    planttype = 1;
+                    state = 1;
+                    Debug.Log("plant potato");
+                }
+                else if (PlayerMovement.main.currentEquip == "carrotseed" && PlayerMovement.main.seed[2] != 0)
+                {
+                    GetComponent<SpriteRenderer>().sprite = plantSprites[2];
+                    planttype = 2;
+                    state = 1;
+                    Debug.Log("plant carrot");
+                }
+                else if (PlayerMovement.main.currentEquip == "broccoliseed" && PlayerMovement.main.seed[3] != 0)
+                {
+                    GetComponent<SpriteRenderer>().sprite = plantSprites[3];
+                    planttype = 3;
+                    state = 1;
+                    Debug.Log("plant broccoli");
+                }
+                else if (PlayerMovement.main.currentEquip == "strawberryseed" && PlayerMovement.main.seed[4] != 0)
+                {
+                    GetComponent<SpriteRenderer>().sprite = plantSprites[4];
+                    planttype = 4;
+                    state = 1;
+                    Debug.Log("plant strawberry");
+                }
+                else if (PlayerMovement.main.currentEquip == "cucumberseed" && PlayerMovement.main.seed[5] != 0)
+                {
+                    GetComponent<SpriteRenderer>().sprite = plantSprites[5];
+                    planttype = 5;
+                    state = 1;
+                    Debug.Log("plant cucumber");
+                }
+                else if (PlayerMovement.main.currentEquip == "cornseed" && PlayerMovement.main.seed[6] != 0)
+                {
+                    GetComponent<SpriteRenderer>().sprite = plantSprites[6];
+                    planttype = 6;
+                    state = 1;
+                    Debug.Log("plant corn");
+                }
             }
+            if(PlayerMovement.main.currentEquip == "watercan" && PlayerMovement.main.watercount > 0)
+            {
+                progress += 1;
+                if(progress == progresslimit && state < 4)
+                {
+                    GetComponent<SpriteRenderer>().sprite = plantSprites[state*7+planttype];
+                    progress = 0;
+                    state += 1;
+                }
+            }
+
         }
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,23 +10,33 @@ public enum PlayerState
 
 public class PlayerMovement : MonoBehaviour
 {
-    public PlayerState currentState;
-    public float speed;
+    public static PlayerMovement main;
+    private PlayerState currentState;
+    [SerializeField]
+    private float speed;
     private Rigidbody2D myRigidBody;
     private Vector3 change;
     private Animator animator;
-    public int currentEquipCount; // barehand, hoe, schyte, watering can
-    public static string currentEquip;
-    string[] equipList = new string[5]{"barehand", "hoe", "schyte", "watercan","seed"};
-    
+    private int currentEquipCount; // barehand, hoe, schyte, watering can
+    public string currentEquip;
+    private readonly string[] equipList = new string[11]{"barehand", "hoe", "schyte", "watercan", "tomatoseed", "potatoseed", "carrotseed", "broccoliseed", "strawberryseed", "cucumberseed", "cornseed" };
+    public int[] seed;
+    public int watercount;
 
 
     // Start is called before the first frame update
+
+    void Awake()
+    {
+        main = this;
+    }
+
     void Start()
     {
         currentState = PlayerState.walk;
         myRigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        Debug.Log(seed[0]+seed[1]+seed[2]+seed[3]);
     }
 
     // Update is called once per frame
@@ -39,8 +49,16 @@ public class PlayerMovement : MonoBehaviour
         if(Input.GetButtonDown("interact") && currentState != PlayerState.interact)
         {
             StartCoroutine(InteractCo());
+            if (currentEquip == "tomatoseed" && seed[0] > 0) seed[0] -= 1;
+            else if (currentEquip == "potatoseed" && seed[1] > 0) seed[1] -= 1;
+            else if (currentEquip == "carrotseed" && seed[2] > 0) seed[2] -= 1;
+            else if (currentEquip == "broccoliseed" && seed[3] > 0) seed[3] -= 1;
+            else if (currentEquip == "strawberryseed" && seed[4] > 0) seed[4] -= 1;
+            else if (currentEquip == "cucumberseed" && seed[5] > 0) seed[5] -= 1;
+            else if (currentEquip == "cornseed" && seed[6] > 0) seed[6] -= 1;
+            else if (currentEquip == "watercan" && watercount > 0) watercount -= 1;
         }
-        else if (Input.GetButtonDown("change equip"))
+        else if(Input.GetButtonDown("change equip"))
         {
             currentEquipCount++;
             currentEquipCount %= equipList.Length;
